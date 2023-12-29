b16c2195a8fe4ce63ada58679cedfa2068edff50
Masked-coder11 <153896221+Masked-coder11@users.noreply.github.com>
2023-12-29 23:45:37 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans(2,-1);
+        
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(target == nums[i]+nums[j]){
+                    ans[0]=i;
+                    ans[1]=j;
+                    return ans;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+};
