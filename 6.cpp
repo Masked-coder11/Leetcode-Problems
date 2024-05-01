42e46ecd2d8ff4bb84cda5847086fb1fa6100891
Masked-coder11 <153896221+Masked-coder11@users.noreply.github.com>
2024-05-01 23:49:07 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string convert(string s, int k) {
+        int diff1=2*k-2, diff2=0;
+        string ans="";
+        int n=s.length();
+        if(k==1 || k>=n){
+            return s;
+        }
+
+        int curr=0;
+        while(curr<n){
+            ans+=s[curr];
+            curr+=diff1;
+        }
+
+        diff1-=2;diff2+=2;
+        for(int i=1;i<=k-2;i++){
+            curr=i;
+            int check=0;
+
+            while(curr<n){
+                ans+=s[curr];
+                if(check==0){
+                    curr+=diff1;
+                    check=1;
+                }
+                else{
+                    curr+=diff2;
+                    check=0;
+                }
+            }
+
+            diff1-=2;
+            diff2+=2;
+        }
+
+        curr=k-1;
+        while(curr<n){
+            ans+=s[curr];
+            curr+=2*k-2;
+        }
+
+        return ans;
+    }
+};
